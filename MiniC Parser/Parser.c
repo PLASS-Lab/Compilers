a3376da445c3d2ab2248adc79889670e6e3b8fcb
PLASS-Lab <48514770+PLASS-Lab@users.noreply.github.com>
2020-11-06 10:15:47 +0900

    Add files via upload

---
+++ MiniC Parser/Parser.c
@@ -0,0 +1,146 @@
+#include "MiniC.tbl"                 /* Mini C table for appendix A */
+//#define  NO_RULES    97            /* number of rules */
+//#define  GOAL_RULE  (NO_RULES+1)   /* accept rule */
+//#define  NO_SYMBOLS  85            /* number of grammar symbols */
+//#define  NO_STATES  153            /* number of states */
+#define  PS_SIZE    100              /* size of parsing stack */
+
+void semantic(int);
+void printToken(struct tokenType token); // -> MiniC Scanner
+void dumpStack();
+void errorRecovery();
+
+int errcnt = 0;
+int sp;                               // stack pointer
+int stateStack[PS_SIZE];              // state stack
+int symbolStack[PS_SIZE];             // symbol stack
+
+void parser()
+{
+	extern int parsingTable[NO_STATES][NO_SYMBOLS + 1];
+	extern int leftSymbol[NO_RULES + 1], rightLength[NO_RULES + 1];
+	int entry, ruleNumber, lhs;
+	int currentState;
+	struct tokenType token;
+	
+	sp = 0; stateStack[sp] = 0;  // initial state
+	token = scanner();
+	while (1) {
+		currentState = stateStack[sp];
+		entry = parsingTable[currentState][token.number];
+		if (entry > 0)                          /* shift action */
+		{
+			sp++;
+			if (sp > PS_SIZE) {
+				printf("critical compiler error: parsing stack overflow");
+				exit(1);
+			}
+			symbolStack[sp] = token.number;
+			stateStack[sp] = entry;
+			token = scanner();
+		}
+		else if (entry < 0)                   /* reduce action */
+		{
+			ruleNumber = -entry;
+			if (ruleNumber == GOAL_RULE) /* accept action */
+			{
+				if (errcnt == 0) printf(" *** valid source ***\n");
+				else  printf(" *** error in source : %d\n", errcnt);
+				return;
+			}
+			semantic(ruleNumber);
+			sp = sp - rightLength[ruleNumber];
+			lhs = leftSymbol[ruleNumber];
+			currentState = parsingTable[stateStack[sp]][lhs];
+			sp++;
+			symbolStack[sp] = lhs;
+			stateStack[sp] = currentState;
+		}
+		else                               /* error action */
+		{
+			printf(" === error in source ===\n");
+			errcnt++;
+			printf("Current Token : ");
+			printToken(token);
+			dumpStack();
+			errorRecovery();
+			token = scanner();
+		}
+	} /* while (1) */
+} /* parser */
+
+void semantic(int n)
+{
+	printf("reduced rule number = %d\n", n);
+}
+
+void dumpStack()
+{
+	int i, start;
+
+	if (sp > 10) start = sp - 10;
+	else start = 0;
+
+	printf("\n *** dump state stack :");
+	for (i = start; i <= sp; ++i)
+		printf(" %d", stateStack[i]);
+
+	printf("\n *** dump symbol stack :");
+	for (i = start; i <= sp; ++i)
+		printf(" %d", symbolStack[i]);
+	printf("\n");
+}
+
+void printToken(struct tokenType token)
+{
+	if (token.number == tident)
+		printf("%s", token.value.id);
+	else if (token.number == tnumber)
+		printf("%d", token.value.num);
+	else
+		printf("%s", tokenName[token.number]);
+
+}
+
+void errorRecovery()
+{
+	struct tokenType tok;
+	int parenthesisCount, braceCount;
+	int i;
+
+	// step 1: skip to the semicolon
+	parenthesisCount = braceCount = 0;
+	while (1) {
+		tok = scanner();
+		if (tok.number == teof) exit(1);
+		if (tok.number == tlparen) parenthesisCount++;
+		else if (tok.number == trparen) parenthesisCount--;
+		if (tok.number == tlbrace) braceCount++;
+		else if (tok.number == trbrace) braceCount--;
+		if ((tok.number == tsemicolon) && (parenthesisCount <= 0) && (braceCount <= 0))
+			break;
+	}
+
+	// step 2: adjust state stack
+	for (i = sp; i >= 0; i--) {
+		// statement_list ->  statement_list .  statement
+		if (stateStack[i] == 36) break;	 // second statement part
+
+		// statement_list ->  .  statement
+		// statement_list ->  .  statement_list statement
+		if (stateStack[i] == 24) break;	 // first statement part
+
+		// declaration_list ->  declaration_list .  declaration
+		if (stateStack[i] == 25) break;  // second internal dcl
+
+		// declaration_list ->  .  declaration
+		// declaration_list ->  .  declaration_list declaration
+		if (stateStack[i] == 17) break;  // internal declaration
+
+		// external declaration
+		// external_dcl ->  .  declaration
+		if (stateStack[i] == 2) break;	// after first external dcl
+		if (stateStack[i] == 0) break;	// first external declaration
+	}
+	sp = i;
+}
